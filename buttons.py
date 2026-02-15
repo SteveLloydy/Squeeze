5aad4aea57f2fceddd21cb737ceecebd483e0c21
Steve Lloyd <steve.lloydy@outlook.com>
2026-02-15 17:54:11 +0000

    test

--- buttons.py
+++ buttons.py
@@ -59,7 +59,7 @@ try:
 			else:
 				counter -= 1
 				direction = "CCW"  # Counter-clockwise
-		print(f"Direction:{direction}|Counter:{counter}")
+			print(f"Direction:{direction}|Counter:{counter}")
 		last_clk_state = clk_state
 		time.sleep(1) # debounce delay            
         
