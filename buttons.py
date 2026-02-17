617e8251b726ae3afdff96da517cc03d95560aa9
SteveLloydy <steve.lloydy@outlook.com>
2026-02-17 13:10:03 +0000

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -14,8 +14,6 @@ from time import sleep
 
 import RPi.GPIO as GPIO
 
-CLK = 23
-DT = 24
 SW = 25
 BTN = 26
 LED = 17
@@ -25,43 +23,24 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED, GPIO.OUT)
 GPIO.output(LED, GPIO.HIGH)
 
-GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+GPIO.setup(BTN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(CLK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-GPIO.setup(DT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 
 # Initial state
-last_clk_state = GPIO.input(CLK)
-last_dt_state = GPIO.input(DT)
 counter = 0
 
-print(f"Last Clock State {last_clk_state}")
-print(f"Last DT State {last_dt_state}")
-time.sleep(2)
 GPIO.output(LED, GPIO.HIGH)
 try:
 	while True:
 		if GPIO.input(BTN) == GPIO.HIGH:
 			print("Button BTN Pressed!")
+			GPIO.output(LED, GPIO.LOW)
+			
 		if GPIO.input(SW) == GPIO.HIGH:
-			print("Button SW Pressed!")
-        
-		clk_state = GPIO.input(CLK)
-		dt_state = GPIO.input(DT)
-		print(f"Clock State {clk_state}")
-		print(f"DT State {dt_state}")
-		time.sleep(1)
-        # Detect rotation
-		if clk_state != last_clk_state:
-			if dt_state != clk_state:
-				counter += 1
-				direction = "CW"  # Clockwise
-			else:
-				counter -= 1
-				direction = "CCW"  # Counter-clockwise
-			print(f"Direction:{direction}|Counter:{counter}")
-		last_clk_state = clk_state
-		time.sleep(1) # debounce delay            
+			print("Button SW Pressed!") 
+			GPIO.output(LED, GPIO.HIGH)    
+		
+		time.sleep(0.0001) # debounce delay    
         
 except KeyboardInterrupt:
 	GPIO.cleanup()
@@ -72,7 +51,6 @@ sleep(10)
 GPIO.output(LED, GPIO.LOW)
 
 GPIO.cleanup()
-
 
 
 
