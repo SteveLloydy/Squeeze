855a252f6689f24a9197c9f1de56e09552bc95dd
SteveLloydy <steve.lloydy@outlook.com>
2026-03-18 11:56:22 +0000

    Update led-circle.py

--- led-circle.py
+++ led-circle.py
@@ -29,9 +29,9 @@ GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Background task function
 def background_loop():
     while True:
-		if GPIO.input(SW) == GPIO.HIGH:
-			print("Button SW Pressed!")
-		time.sleep(0.05)
+        if GPIO.input(SW) == GPIO.HIGH:
+            print("Button SW Pressed!")
+        time.sleep(0.05)
 		
 # Create and start a daemon thread
 thread = threading.Thread(target=background_loop, daemon=True)
